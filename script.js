de909651ccf8430231f862222ec416ba5161d19f
suranjan chaudhari <suranjanchaudhari07@gmail.com>
2026-02-04 14:27:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Portfolio Loaded Successfully 🚀");
+// Navbar Toggle (Mobile)
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
