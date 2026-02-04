a3aefb877fca3fb89a0f78218d95d773353ce2a7
suranjan chaudhari <suranjanchaudhari07@gmail.com>
2026-02-04 14:51:47 +0530

    Implement close menu on link click
    Added functionality to close the navigation menu when a link is clicked.

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navMenu = document.getElementById("nav-menu");
 menuBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
+
+
+// Close menu on click (mobile)
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+  });
+});
